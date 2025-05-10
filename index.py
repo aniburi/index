e46913858032b34d3c11bce720c1732b5b224cc2
aniburi <rizvijhpi@gmail.com>
2025-05-10 13:24:29 +0600

    Update index.txt

--- index.py
+++ index.py
@@ -1,11 +1,1 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler
-
-TOKEN = "8133649039:AAFLpgxN8TmzEjszY5zjoGaPX5wpkR8rKgk"
-
-async def start(update: Update, context):
-    await update.message.reply_text('স্বাগতম! টোকেন উপার্জন শুরু করুন!')
-
-app = Application.builder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+don'  command 
